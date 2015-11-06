3bd549063b426987a68d8fee68bf63c0fa6bbb4d
Alex Glenn-Murray <alex.glenn.murray@gmail.com>
2015-11-06 13:34:10 -0800

    Many updates; now interactive.

---
+++ game.lua
@@ -0,0 +1,76 @@
+local game = {}
+
+function game.new(w, h)
+  local map = { w = w, h = h }
+  for i = 1, w do
+    map[i] = {}
+    for j = 1, h do
+      map[i][j] = 0
+    end
+  end
+  return map
+end
+
+function game.random(w, h)
+  local map = game.new(w, h)
+  for i = 1, w do
+    for j = 1, h do
+      if math.random(10) == 1 then
+        map[i][j] = 1
+      end
+    end
+  end
+  return map
+end
+
+-- neighbors table
+local dirs = { {-1,-1}, {0,-1}, {1,-1}, {-1,0}, {1,0}, {-1,1}, {0,1}, {1,1} }
+
+function game.getNeighbors(map, i, j)
+  local w, h = map.w, map.h
+  local v = 0
+  -- iterate adjacent cells
+  for _, dir in ipairs(dirs) do
+    local i2, j2 = i + dir[1], j + dir[2]
+    -- check if the neighbor is off the map
+    if i2 >= 1 and i2 <= w then
+      if j2 >= 1 and j2 <= h then
+        v = v + map[i2][j2]
+      end
+    end
+  end
+  return v
+end
+
+-- rules table
+local rules = {}
+-- dead cells become alive if there are 3 live neighbors
+rules[0] = { [0] = 0, 0, 0, 1, 0, 0, 0, 0, 0 }
+-- live cells survive if there are 2 or 3 live neighbors
+rules[1] = { [0] = 0, 0, 1, 1, 0, 0, 0, 0, 0 }
+
+function game.step(map)
+  local w, h = map.w, map.h
+  local map2 = { w = w, h = h }
+  for i = 1, w do
+    map2[i] = {}
+    for j = 1, h do
+      local s = map[i][j]
+      local n = game.getNeighbors(map, i, j)
+      map2[i][j] = rules[s][n]
+    end
+  end
+  return map2
+end
+
+function game.getPopulation (map)
+  pop = 0
+  for i = 1, map.w do
+    for j = 1, map.h do
+      pop = pop + map[i][j]
+    end
+  end
+  return pop
+end
+
+return game
--- main.lua
+++ main.lua
@@ -1,20 +1,86 @@
-local cells = require "cells"
+local w, h = 120, 80
+local dw, dh = love.graphics.getDimensions()
+local cw, ch = dw/w, dh/h
+
+local game = require("game")
+local map = game.random(w, h)
+local paused = true
+
+function love.keypressed (key)
+  if key == ' ' or key == 'space' then
+    if paused == true then
+      paused = false
+    elseif paused == false then
+      paused = true
+    end
+  end
+  return paused
+end
+
+function mouseDraw ()
+  -- edit map using the mouse
+  local x, y = love.mouse.getPosition()
+  local x2 = math.ceil(x/cw)
+  local y2 = math.ceil(y/ch)
+  if love.mouse.isDown('l') then
+    map[x2][y2] = 1
+    paused = true
+  elseif love.mouse.isDown('r') then
+    map[x2][y2] = 0
+    paused = true
+  else
+    --paused = false
+  end
+  return x2, y2
+end
 
 function love.load ()
-  cells.load ()
+  time = 0
 end
 
-function love.update (dt)
-  cells.update ()
-  love.timer.sleep( .1 )
-end
-
-function love.draw ()
-  cells.draw ()
-  cells.print ()
-  --for i = 1, cells.x do
-  --  for j = 1, cells.y do
-  --    love.graphics.print (tostring (cells.map[i][j]), 0 + (10 * i) - 10, 0 + (10 * j) - 10)
-  --  end
+function love.update(dt)
+  -- edit map using the mouse
+  --local x, y = love.mouse.getPosition()
+  --local x2 = math.ceil(x/cw)
+  --local y2 = math.ceil(y/ch)
+  --if love.mouse.isDown('l') then
+  --  map[x2][y2] = 1
+  --  paused = true
+  --elseif love.mouse.isDown('r') then
+  --  map[x2][y2] = 0
+  --  paused = true
+  --else
+  --  --paused = false
   --end
+  mouseDraw ()
+  -- update the map
+  if not paused then
+    map = game.step(map)
+    time = time + 1
+  end
+end
+
+function love.draw()
+  for i = 1, w do
+    for j = 1, h do
+      if map[i][j] == 1 then
+        love.graphics.setColor (0,255,255)
+        love.graphics.rectangle('fill', cw*i - cw, ch*j - ch, cw, ch)
+      end
+    end
+  end
+
+  love.graphics.setColor (255,0,0)
+  --if paused == false then
+    local x, y = love.mouse.getPosition()
+    local x2 = math.ceil(x/cw)
+    local y2 = math.ceil(y/ch)
+    love.graphics.rectangle ('line', x2*(cw)-cw,y2*(ch)-ch, ch,cw)
+  --end
+  love.graphics.setColor (255,255,255)
+  love.graphics.print ("population "..game.getPopulation (map), 10, 10)
+  love.graphics.print ("generations "..time, 10, 20)
+  if paused == true then
+    love.graphics.print ("Press space to pause or unpause, left mouse button to add living cells, and right mouse to 'kill' cells", 10, 30)
+  end
 end
